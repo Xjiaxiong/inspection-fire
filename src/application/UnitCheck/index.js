e7e426c40af279e28dd1b1de7c7ccf4826851450
许家熊 <xujiaxiong@qq.com>
2021-03-21 18:24:11 +0800

    线上稳定版本

--- src/application/UnitCheck/index.js
+++ src/application/UnitCheck/index.js
@@ -19,16 +19,18 @@ import { getWrapParams, GetQuery } from '../../api/utils'
 import showLoading from 'gdt-jsapi/showLoading'
 import hideLoading from 'gdt-jsapi/hideLoading'
 import toast from 'gdt-jsapi/toast'
-
+let imgTemps = []
 function UnitCheck(props) {
     const querys = GetQuery(props.location.search)
     const [checksSubmit, setChecksSubmit] = useState([])
+    // const [imgTemps, setImgTemps] = useState([])
     const [partInfo, setPartInfo] = useState(null)
 
     //通过异步函数拿到当前页面的数据
 
     useEffect(() => {
         if(!partInfo) {
+            imgTemps = [];
             getCheckTemp()
         }
         // eslint-disable-next-line
@@ -109,6 +111,97 @@ function UnitCheck(props) {
         })
         setChecksSubmit(list)
     }
+    const upload = async (dataURL,index) => {
+        let fd = new FormData();
+        let blob = dataURItoBlob(dataURL);
+        fd.append('file', blob);
+        let resImg = await upImgRequest(fd)
+        const data = resImg.data[0]
+        let logoID = data && data.newFileName
+        // let imgsPre = [...imgTemps];
+        imgTemps.push({
+            "uuid": index,
+            "fattach_index": 1,
+            "fattach_link": logoID,
+            "fattach_name": logoID
+        })
+        // console.log("upload",data)
+        // setImgTemps(imgsPre)
+    }
+    const compress = (img) =>  {
+          // 用于压缩图片的canvas
+        var canvas = document.createElement("canvas");
+        var ctx = canvas.getContext('2d');
+
+        // 瓦片canvas
+        var tCanvas = document.createElement("canvas");
+        var tctx = tCanvas.getContext("2d");
+
+        var initSize = img.src.length;
+        var width = img.width;
+        var height = img.height;
+
+        //如果图片大于四百万像素，计算压缩比并将大小压至400万以下
+        var ratio;
+        if ((ratio = width * height / 4000000)>1) {
+            ratio = Math.sqrt(ratio);
+            width /= ratio;
+            height /= ratio;
+        }else {
+            ratio = 1;
+        }
+
+        canvas.width = width;
+        canvas.height = height;
+
+        //铺底色
+        ctx.fillStyle = "#fff";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        //如果图片像素大于100万则使用瓦片绘制
+        var count;
+        if ((count = width * height / 1000000) > 1) {
+            count = ~~(Math.sqrt(count)+1); //计算要分成多少块瓦片
+
+        //计算每块瓦片的宽和高
+            var nw = ~~(width / count);
+            var nh = ~~(height / count);
+
+            tCanvas.width = nw;
+            tCanvas.height = nh;
+
+            for (var i = 0; i < count; i++) {
+                for (var j = 0; j < count; j++) {
+                    tctx.drawImage(img, i * nw * ratio, j * nh * ratio, nw * ratio, nh * ratio, 0, 0, nw, nh);
+
+                    ctx.drawImage(tCanvas, i * nw, j * nh, nw, nh);
+                }
+            }
+        } else {
+            ctx.drawImage(img, 0, 0, width, height);
+        }
+
+        //进行最小压缩
+        var ndata = canvas.toDataURL('image/jpeg', 0.1);
+
+        console.log('压缩前：' + initSize);
+        console.log('压缩后：' + ndata.length);
+        console.log('压缩率：' + ~~(100 * (initSize - ndata.length) / initSize) + "%");
+
+        tCanvas.width = tCanvas.height = canvas.width = canvas.height = 0;
+
+        return ndata;
+    }
+    const dataURItoBlob = (dataURI) => {
+        var byteString = atob(dataURI.split(',')[1])
+        var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+        var ab = new ArrayBuffer(byteString.length)
+        var ia = new Uint8Array(ab)
+        for (var i = 0; i < byteString.length; i++) {
+            ia[i] = byteString.charCodeAt(i)
+        }
+        return new Blob([ab], {type: mimeString})
+    }
     const submit = async ()=> {
         showLoading({text:'提交中...'})
         let flist = [];
@@ -123,24 +216,59 @@ function UnitCheck(props) {
                 fis_vote: item.fis_vote,
                 ftotal_score: item.total_score,
                 fdscore: item.score,
+                fattach_list:[]
             }
-            let fattach_list = [];
-            for(let j=0; j<item.imgs.length;j++) {
-                let img = item.imgs[j];
-                let formData  = new FormData();
-                formData.append("file", img.file);
-                let resImg = await upImgRequest(formData)
-                const data = resImg.data[0]
-                let logoID = data && data.newFileName //获取上传以后的图片ID
-                fattach_list.push({
-                    "fattach_index": j,
-                    "fattach_link": logoID,
-                    "fattach_name": logoID
+            let uuid = item.uuid;
+            if(item.imgs.length > 0) {
+                //图片压缩上传
+                item.imgs.forEach(imgObj => {
+                    // if(item.file.type)
+                    let reader = new FileReader();
+                    reader.readAsDataURL(imgObj.file);
+                    reader.onloadend = function () {
+                        var result = this.result;
+                        var img = new Image();
+                        img.src = result;
+                        //如果图片大小小于200kb，则直接上传
+                        if (result.length <= 200*1024) {
+                            img = null;
+                            upload(result,uuid);
+                            return;
+                        }
+                        //图片加载完毕之后进行压缩，然后上传
+                        if (img.complete) {
+                            callback();
+                        } else {
+                            img.onload = callback;
+                        }
+                        function callback() {
+                            var data = compress(img);
+                            upload(data,uuid);
+                            img = null;
+                        }
+                    };
                 })
+
             }
-            obj.fattach_list = fattach_list
             flist.push(obj)
         }
+        setTimeout(() => {
+            doSubmit(flist)
+        },3000)
+
+    }
+
+    const doSubmit = async (flist) => {
+        console.log('imgTemps-----',imgTemps)
+        flist.forEach(item => {
+            for(let j=0; j<imgTemps.length;j++) {
+                let img = imgTemps[j];
+                if(img.uuid === item.fgridmodeldtl_uuid) {
+                    item.fattach_list.push(img)
+                }
+            }
+        })
+
         let params = {
             fgridmodelmain_uuid: querys.fgridmodelmain_uuid, 
             fsocial_uuid: querys.fsocial_id,
@@ -164,7 +292,6 @@ function UnitCheck(props) {
                 text: "提交失败"
             })
         }
-        
     }
 
     
@@ -218,7 +345,7 @@ function UnitCheck(props) {
                                                 files={item.imgs}
                                                 onChange={(files, type) => onChangeFiles(files, type, item.uuid)}
                                                 onImageClick={(index, fs) => console.log(index, fs)}
-                                                selectable={item.imgs.length < 3}
+                                                selectable={item.imgs.length < 2}
                                             />
                                         </div> 
                                     </div>
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
 const proxy = require('http-proxy-middleware');
-const locService = 'http://192.168.0.106:9010'
+const locService = 'http://192.168.0.101:9010'
 const testService = 'http://111.0.89.21:9010/'
 const proService = 'https://czw.menhey.cn'
 const mockService = 'http://localhost:3001'
